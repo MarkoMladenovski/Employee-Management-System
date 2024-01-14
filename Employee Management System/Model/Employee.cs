c7571d2547286762d77bed5dcf99a3293a2d7ffa
Marko Mladenovski <m.mladenovski24@hotmail.com>
2024-01-14 03:20:44 +0100

    Added Controller class

--- Employee Management System/Model/Employee.cs
+++ Employee Management System/Model/Employee.cs
@@ -1,4 +1,6 @@
-﻿public class Employee
+﻿namespace EmployeeNamespace.Model
+
+public class Employee
 {
     public int EmployeeId { get; set; }
     public string Name { get; set; }
--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Employee.Model; 
+using EmployeeNamespace.Model; 
 
 public class Startup
 {
